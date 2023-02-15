bbc03a9746e0a9b73c07e2dbc2202b2120780628
Caitlin Jordan <jordan.cait17@gmail.com>
2023-02-14 19:58:55 -0800

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//const titleCased = () => {
+//  return tutorials
+//}
+
+const titleCased = function() {
+  return (tutorials.map(string => string.split(' ').map(function(word){
+      return (word.charAt(0).toUpperCase() + word.slice(1));
+    }).join(' ')))
 }
